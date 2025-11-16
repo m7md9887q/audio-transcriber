15fe3a83b78a8f9f704bb9dbda8bd22a8f648564
m7md9887q <mmd412846@gmail.com>
2025-11-17 00:31:17 +0300

    Fix Render PORT binding

--- app.py
+++ app.py
@@ -115,5 +115,7 @@ def transcribe():
             pass
 
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
